a6fc82c10b878a905983263d3d8fcb3693d041b5
Arush2510 <121389635+Arush2510@users.noreply.github.com>
2023-07-02 20:45:38 +0530

    Add files via upload

---
+++ Assigning values to variables basics.R
@@ -0,0 +1,14 @@
+# Assigning values to variables and performing functions
+x <- 3 * 4
+x
+y <- x - 4
+y
+z <- x - y
+z
+# Variables are case sensetive
+Z # this wont show the value of "z"
+my_variable <- x+y+z*(x*y /z)
+my_variable
+trigo <- tan(pi/4)
+a <- trigo*3
+a
